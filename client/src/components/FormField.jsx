ce0b59117b307647c0d391061ec132c0b4ca824e
Captain F Harlock <info@mbdfinancials.com>
2023-01-29 23:53:09 +0100

    update API.

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -14,7 +14,7 @@ const FormField = ({
     <div className="flex items-center gap-2 mb-2">
       <label
         htmlFor={name}
-        className="block text-sm font-medium text-gray-900"
+        className="block text-sm font-medium text-white"
       >
         {labelName}
       </label>
